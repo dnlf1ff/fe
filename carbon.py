14ca6a85dd15601a455fed5001654c3679b0e4ac
YutackPark <yutackpark@snu.ac.kr>
2025-03-11 18:03:27 +0900

    ditchinglexicon..

--- carbon.py
+++ carbon.py
@@ -11,21 +11,21 @@ conf=taskCONF('.')
 def create_sc(a, mlp)
     fe_unit=BodyCenteredCubic(directions=[1,0,0],[0,1,0],[0,0,1]], size=(1,1,1), symbol='Fe', pbc=True, latticeconstant=a)
     fe_sc=fe_unit.repeat((4,4,4))
-    write(f'poscar/{mlp}_sc.poscar',sc, format='vasp')
+    write(f'structure/{mlp}/{mlp}_sc.poscar',sc, format='vasp')
     return fe_sc
 
 def a1(fe,a,mlp):
     fe_a1 = fe.copy()
     del fe_a1[1]
     fe_a1.append(Atoms('C', positions=[(a/2,a/2,0)]))
-    write(f'poscar/{mlp}_a1.poscar',fe_a1, format='vasp')
+    write(f'structure/{mlp}/{mlp}_a1.poscar',fe_a1, format='vasp')
     return fe_a1
 
 def a2(fe,a,mlp):
     fe_a2 = fe.copy()
     del fe_a2[1]
     fe_a2.append(Atoms('C', positions=[(0,0,a/2)]))
-    write(f'poscar/{mlp}_a2.poscar',fe_a2,format='vasp')
+    write(f'structure/{mlp}/{mlp}_a2.poscar',fe_a2,format='vasp')
     return fe_a2
 
 #TODO: b, c
@@ -34,7 +34,7 @@ def b1(fe, a, mlp):
     del fe_b1[1]
     del fe_b1[3]
     fe_b1.append(Atoms('C', positions=[(a/2,a/2,a)])))
-    write(f'poscar/{mlp}_b1.poscar',fe_b1,format='vasp')
+    write(f'structure/{mlp}/{mlp}_b1.poscar',fe_b1,format='vasp')
     return fe_b1
 
 def b2(fe, a, mlp):
@@ -42,7 +42,7 @@ def b2(fe, a, mlp):
     del fe_b2[1]
     del fe_b2[3]
     fe_b2.append(Atoms('C',positions=[(0,0,a/2)]))
-    write(f'poscar/{mlp}_b2.poscar',fe_b2,format='vasp')
+    write(f'structure/{mlp}/{mlp}_b2.poscar',fe_b2,format='vasp')
     return fe_b2
 
 def b3(fe, a, mlp):
@@ -50,7 +50,7 @@ def b3(fe, a, mlp):
     del fe_b3[1]
     del fe_b3[3]
     fe_b3.append(Atoms('C',positions=[(a/2,a/2,0)])
-    write(f'poscar/{mlp}_b3.poscar', fe_b3, format='vasp')
+    write(f'structure/{mlp}/{mlp}_b3.poscar', fe_b3, format='vasp')
     return fe_b3
 
 def c1(fe, a, mlp):
@@ -58,7 +58,7 @@ def c1(fe, a, mlp):
     del fe_c1[0]
     del fe_c1[1]
     fe_c1.append(Atoms('C',positions=[(a/2,a/2,a)]))
-    write(f'poscar/{mlp}_c1.poscar',fe_c1, format='vasp')
+    write(f'structure/{mlp}/{mlp}_c1.poscar',fe_c1, format='vasp')
     return fe_c1
 
 
@@ -67,7 +67,7 @@ def c2(fe, a, mlp):
     del fe_c2[0]
     del fe_c2[1]
     fe_c1.append(Atoms('C',positions=[(a/2,a/2,0)]))
-    write(f'poscar/{mlp}_c2.poscar',fe_c2 format='vasp')
+    write(f'structure/{mlp}/{mlp}_c2.poscar',fe_c2 format='vasp')
     return fe_c2
 
 
@@ -75,7 +75,7 @@ def d(fe, a, mlp):
     fe_d = fe.copy()
     fe_d.append(Atoms('C', positions=[(a/2,a/2,0)]))
     fe_d.append(Atoms('C', positions=[(a/2,a/2,a)]))
-    write(f'poscar/{mlp}_d.poscar',fe_d, format='vasp')
+    write(f'structure/{mlp}/{mlp}_d.poscar',fe_d, format='vasp')
     return fe_d
 
 
@@ -83,41 +83,41 @@ def e(fe,a,mlp):
     fe_e = fe.copy()
     fe_e.append(Atoms('C', positions=[(a/2,a/2,a)]))
     fe_e.append(Atoms('C', positions=[(a/2,a,a/2)]))
-    write(f'poscar/{mlp}_e.poscar',fe_e, format='vasp')
+    write(f'structure/{mlp}/{mlp}_e.poscar',fe_e, format='vasp')
     return fe_e
 
 def f(fe, a, mlp):
     fe_f=fe.copy()
     fe.append(Atoms('C',positions=[(0,a/2,0)]))
     fe.append(Atoms('C',positions=[(a,3*a/2,a)]))
-    write(f'poscar/{mlp}_f.poscar',fe_f,format='vasp')
+    write(f'structure/{mlp}/{mlp}_f.poscar',fe_f,format='vasp')
 
 def i(fe,a,mlp):
     fe_i = fe.copy()
     fe_i.append(Atoms('C', positions=[(a/2,a/2,a)]))
     fe_i.append(Atoms('C', positions=[(0,0,a/2)]))
-    write(f'poscar/{mlp}_i.poscar',fe_i,format='vasp')
+    write(f'structure/{mlp}/{mlp}_i.poscar',fe_i,format='vasp')
     return fe_i
 
 def g(fe,a,mlp):
     fe_g = fe.copy()
     fe_g.append(Atoms('C', positions=[(0,0,a/2)]))
     fe_g.append(Atoms('C', positions=[(0,a,a/2)]))
-    write(f'poscar/{mlp}_g.poscar',fe_g,format='vasp')
+    write(f'structure/{mlp}/{mlp}_g.poscar',fe_g,format='vasp')
     return fe_g
 
 def h(fe, a, mlp):
     fe_h=fe.copy()
     fe_h.append(Atoms('C',positions=[(a/2,a/2,a)]))
     fe_h.append(Atoms('C',positions=[(a/2,a,a/2)]))
-    write(f'poscar/{mlp}_h.poscar', fe_h, format='vasp')
+    write(f'structure/{mlp}/{mlp}_h.poscar', fe_h, format='vasp')
 
 def j(fe,a,mlp):
     fe_j = fe.copy()
     del fe_j[1]
     fe_j.append(Atoms('C', positions=[(a/2,a/2,0)]))
     fe_j.append(Atoms('C', positions=[(a/2,a/2,a)]))
-    write(f'poscar/{mlp}_j.poscar',fe_j,format='vasp')
+    write(f'structure/{mlp}/{mlp}_j.poscar',fe_j,format='vasp')
     return fe_j
 
 def k(fe,a,mlp):
@@ -125,7 +125,7 @@ def k(fe,a,mlp):
     del fe_k[1]
     fe_k.append(Atoms('C', positions=[(a/2,a/2,0)]))
     fe_k.append(Atoms('C', positions=[(0,a/2,a/2)]))
-    write(f'poscar/{mlp}_k.poscar',fe_k,format='vasp')
+    write(f'structure/{mlp}/{mlp}_k.poscar',fe_k,format='vasp')
     return fe_k
 
 def l(fe,a,mlp):
@@ -133,7 +133,7 @@ def l(fe,a,mlp):
     del fe_l[1]
     fe_l.append(Atoms('C', positions=[(a/2,a/2,a/4)]))
     fe_l.append(Atoms('C', positions=[(a/2,a/2,a*3/4)]))
-    write(f'poscar/{mlp}_l.poscar',fe_l,format='vasp')
+    write(f'structure/{mlp}/{mlp}_l.poscar',fe_l,format='vasp')
     return fe_l
 
 def m(fe,a,mlp):
@@ -141,26 +141,32 @@ def m(fe,a,mlp):
     del fe_m[1]
     fe_m.append(Atoms('C', positions=[(a/2,a/4,a*3/4)]))
     fe_m.append(Atoms('C', positions=[(a/2,a*3/4,a/4)]))
-    write(f'poscar/{mlp}_m.poscar',fe_m,format='vasp')
+    write(f'structure/{mlp}/{mlp}_m.poscar',fe_m,format='vasp')
     return fe_m
 
-def vac_1(fe,mlp):
-    fe_vac1=fe.copy()
-    del fe_vac1[1]
-    write(f'poscar/{mlp}_vac1.posar',fe_vac1, format='vasp')
-    return fe_vac1
 
-def vac_13(fe,mlp)
-    fe_vac13=fe.copy()
-    del fe_vac13[1]
-    del ve_vac13[3]
-    write(f'poscsar/{mlp}_vac13.poscar',fe_vac13,format='vasp')
+def vacency(name, fe, mlp, inp):
+    fe_vac=fe.copy()
+    p=inp[name]['p']
+    if p==2:
+        vac=inp[name]['vac']
+        del fe_vac[vac[0]]
+        del fe_vac[vac[1]]
+        tag='13' if 3 in vac else '01'       
+        write(f'structure/{mlp}/{mlp}_vac{tag}.mlp',fe_vac, format='vasp')
+        return fe_vac
+    elif p==1:
+        del fe_vac[p]
+        tag=str(p)
+        write(f'structure/{mlp}/{mlp}_vac{tag}.mlp',fe_vac, format='vasp')
+        return fe_vac
+    else:
+        raise ValueError("p, number/index of vacancy/ies should be a integer/list of integers")
 
-    return vac_13
-
-
-def
-
+def carbon_interstitial(name, fe, mlp, inp):
+    fe_c=fe.copy()
+    q=inp[name]['q']
+    q_pos=inp[name]['q_pos']
 
 def relax(atoms, runner):
     atoms.calc=runner.calculator
